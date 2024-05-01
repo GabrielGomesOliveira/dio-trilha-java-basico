1534987fa48f0e37c8c15531b09ee07ee100a27e
Gabriel Gomes <gabriel.oliveira1405@outlook.com>
2024-05-01 15:06:26 -0300

    Aplicando aprendizado sobre tipos de váriaveis

---
+++ Tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,14 @@
+public class TipoVariaveis {
+    public static void main(String[] args) throws Exception {
+       //Tipos primitivos
+       //Estudando a classe string que representa texto na aplicação 
+
+       String meuNome = "Gabriel Gomes";
+
+       double salarioMinimo = 2500;
+       short numeroCurto = 1;
+       int numero = 5;
+        final double VALOR_DE_PI = 3.14;
+
+    }
+}
